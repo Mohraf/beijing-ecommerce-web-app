c3838934a77dde4a5dadbd0215ca7155591b10a3
mohraf <amosngok@gmail.com>
2022-07-13 14:43:21 +0300

    remove cart items

--- js/main.js
+++ js/main.js
@@ -5,10 +5,31 @@ let closeCart = document.querySelector('#close-cart');
 
 // Open Cart
 cartIcon.onclick = () => {
-    cart.classList.add('active')
+    cart.classList.add('active');
 }
 
 // Close cart
 closeCart.onclick = () => {
-    cart.classList.remove('active')
+    cart.classList.remove('active');
+}
+
+// Cart working JS
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+// Making function
+function ready(){
+    // Remove items from cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i]
+        button.addEventListener('click', (e) => {
+            var buttonClicked = e.target
+            buttonClicked.parentElement.remove()
+        })
+    }
 }
